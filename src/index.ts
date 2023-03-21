af88a70957bdaf44d6e0880b09584d47437ba620
Gustavo Harff <gustavo.harff@gmail.com>
2023-03-21 19:51:33 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,39 @@ const result: Result[] = []
 
 const lines = file.split("\n");
 
-const regex = /(int|double|float|char)\ ([A-Z][a-z]*)/ 
+const numbers = /\d+,?\d+/g
+
+const reservadas = /(int|double|float|char|while|for|if)/
+const identificador = /([A-Z][a-z]*)/
 
 lines.forEach((line, index) => {
-  if (line.match(regex)) {
-    if (line.match(regex)?.[2]) {
+  if (line.match(reservadas)) {
+    if (line.match(reservadas)?.[1]) {
       result.push({
         line: index + 1,
-        value: line.match(regex)?.[2] || '',
+        value: line.match(reservadas)?.[1] || '',
+        type: 'reservada'
+      })
+    }
+  }
+
+  if (line.match(identificador)) {
+    if (line.match(identificador)?.[1]) {
+      result.push({
+        line: index + 1,
+        value: line.match(identificador)?.[1] || '',
         type: 'identificador'
       })
     }
+  }
 
-    if (line.match(regex)?.[1]) {
+  if (line.match(numbers)) {
+    console.log(line.match(numbers))
+    if (line.match(numbers)?.[0]) {
       result.push({
         line: index + 1,
-        value: line.match(regex)?.[1] || '',
-        type: 'reservada'
+        value: line.match(numbers)?.[0] || '',
+        type: 'number'
       })
     }
   }
